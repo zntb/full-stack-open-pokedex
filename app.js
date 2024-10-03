48b67086f15ecf611c2068d90c3075e7402add7d
zntb <zntbdev@proton.me>
2024-10-03 11:56:29 +0300

    Add finished exercise 11.6

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+import express from 'express'
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
+})
