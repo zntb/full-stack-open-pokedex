666c34eb3d38817da3e34d7a54581a55ae5f2ac6
zntb <zenetibi@yahoo.com>
2024-10-04 11:13:15 +0300

    Simulate a broken deployment

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/healthz', (req, res) => {
+// eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
