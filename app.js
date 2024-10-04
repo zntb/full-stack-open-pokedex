507174ff1b2a1463f54bac7fc1eb438913548f5c
zntb <zenetibi@yahoo.com>
2024-10-04 11:40:00 +0300

    Add finished exercise 11.12

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/healthz', (req, res) => {
-// eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
